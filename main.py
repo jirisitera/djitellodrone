88dfb7a484e04af54a28347090c9f9c1a338a86a
jirisitera <jirik.sitera@gmail.com>
2025-09-13 16:31:44 +0200

    major upgrades

--- main.py
+++ main.py
@@ -1,48 +1,89 @@
 from djitellopy import tello
-import time
 import cv2
-import keyboard
-global img
-def getKeyboardInput():
-    #LEFT RIGHT, FRONT BACK, UP DOWN, YAW VELOCITY
-    lr, fb, ud, yv = 0, 0, 0, 0
-    speed = 80 
-    liftSpeed = 80
-    moveSpeed = 85
-    rotationSpeed = 100
-    if keyboard.is_pressed('left'):
-        lr = -speed
-    if keyboard.is_pressed('right'):
-        lr = speed
-    if keyboard.is_pressed('up'):
-        fb = moveSpeed
-    if keyboard.is_pressed('down'):
-        fb = -moveSpeed
-    if keyboard.is_pressed('w'):
-        ud = liftSpeed
-    if keyboard.is_pressed('s'):
-        ud = -liftSpeed
-    if keyboard.is_pressed('d'):
-        yv = rotationSpeed
-    if keyboard.is_pressed('a'):
-        yv = -rotationSpeed
-    if keyboard.is_pressed('q'):
-        Drone.land()
-        time.sleep(3)
-    if keyboard.is_pressed('e'):
-        Drone.takeoff()
-    if keyboard.is_pressed('z'):
-        cv2.imwrite(f"Drone/Images/{time.time()}.jpg", img)
-        time.sleep(0.3)
-    return [lr, fb, ud, yv]
-Drone = tello.Tello()
-Drone.connect()
-print(Drone.get_battery())
-Drone.streamon()
-while True:
-    keyValues = getKeyboardInput()
-    Drone.send_rc_control(keyValues[0], keyValues[1], keyValues[2], keyValues[3])
-    img = Drone.get_frame_read().frame
-    img = cv2.resize(img, (1080, 720))
-    cv2.imshow("DroneCapture", img)
-    cv2.waitKey(1)
+import pygame
+import numpy as np
+import time
+import os
+import sys
+FPS = 120
+os.environ["SDL_VIDEO_ALLOW_SCREENSAVER"] = "1"
+os.environ["SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"] = "1"
+os.environ["SDL_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR"] = "0"
+class joystick_handler(object):
+    def __init__(self, id):
+        self.id = id
+        self.joy = pygame.joystick.Joystick(id)
+        self.name = self.joy.get_name()
+        self.joy.init()
+        self.numaxes = self.joy.get_numaxes()
+        self.axis = []
+        for i in range(self.numaxes):
+            self.axis.append(self.joy.get_axis(i))
+        self.numbuttons = self.joy.get_numbuttons()
+        self.button = []
+        for i in range(self.numbuttons):
+            self.button.append(self.joy.get_button(i))
+        self.hat = []
+class app(object):
+    def init(self):
+        pygame.init()
+        pygame.display.set_caption("Wireless Drone Controller")
+        self.screen = pygame.display.set_mode([960, 720])
+        pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
+        pygame.event.set_blocked((pygame.MOUSEMOTION, pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN))
+        self.joycount = pygame.joystick.get_count()
+        if self.joycount == 0:
+            print("This program only works with at least one joystick plugged in. No joysticks were detected.")
+            self.quit(1)
+        self.joy = []
+        for i in range(self.joycount):
+            self.joy.append(joystick_handler(i))
+    def run(self):
+        lr, fb, ud, yv = 0, 0, 0, 0
+        Drone.streamoff()
+        Drone.streamon()   
+        while True:
+            frame_read = Drone.get_frame_read()
+            if frame_read.stopped: break
+            self.screen.fill([0, 0, 0])
+            frame = frame_read.frame
+            text = "Battery: {}%".format(Drone.get_battery())
+            cv2.putText(frame, text, (5, 720 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = np.rot90(frame)
+            frame = np.flipud(frame)
+            frame = pygame.surfarray.make_surface(frame)
+            self.screen.blit(frame, (0, 0))
+            pygame.display.update()
+            time.sleep(1 / FPS)
+            speed = 80 
+            liftSpeed = 40
+            moveSpeed = 90
+            rotationSpeed = 100
+            for event in [pygame.event.wait()] + pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION:
+                    self.joy[event.joy].axis[event.axis] = event.value
+                    if event.axis == 0:
+                        lr = int(event.value * speed)
+                    elif event.axis == 1:
+                        fb = -int(event.value * moveSpeed)
+                    elif event.axis == 2:
+                        yv = int(event.value * rotationSpeed)
+                    elif event.axis == 3:
+                        ud = -int(event.value * liftSpeed)
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    self.joy[event.joy].button[event.button] = 1
+                    if event.button == 0:
+                        Drone.land()
+                    if event.button == 3:
+                        Drone.takeoff()
+            Drone.send_rc_control(lr, fb, ud, yv)
+    def quit(self, status=0):
+        pygame.quit()
+        sys.exit(status)
+if __name__ == "__main__":
+    Drone = tello.Tello()
+    Drone.connect()
+    program = app()
+    program.init()
+    program.run()
